88e2ae19ecc6d9fff5eaf843e7f73a3da75292dc
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-03-25 23:14:47 -0400

    added blurb

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,23 @@ import Accordion_ from "../components/Accordion_";
 export default function About() {
   return (
     <>
+      <div id="parent">
+        <div>
+          <div className="image-container-about">
+            <img
+              src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/01/boston-skyline-best-views-socail.jpg"
+              alt="bostonskyline"
+              className="image"
+            ></img>
+          </div>
+        </div>
+        <h2 className='image-text'>About</h2>
+        <hr className='image-text'/ >
+        <p className='image-text'>Partnered with the BHC, our team at Scout Labs is dedicated to creating an effective homebuying guide for first time home buyers in Boston! </p>
+        <div className="blurb">
+          <Accordion_ />
+        </div>
+      </div>
       <Accordion_ />
     </>
   );
--- src/pages/HomebuyingGuide.jsx
+++ src/pages/HomebuyingGuide.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import "../styles/app.css";
 
-export default function HomebuyingGuide(){
-
-    return (
-    <div class="image-container">
-    <img
-      src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/01/boston-skyline-best-views-socail.jpg"
-      alt="bostonskyline"
-      className="image"
-    ></img>
-  </div>);
+export default function HomebuyingGuide() {
+  return (
+    <div className="image-container">
+      <img
+        src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/01/boston-skyline-best-views-socail.jpg"
+        alt="bostonskyline"
+        className="image"
+      ></img>
+    </div>
+  );
 }
