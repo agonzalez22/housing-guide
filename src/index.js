90960416a5d8df6530f45664fb0b0be9b6facba5
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-03-25 23:48:35 -0400

    testing routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.css";
 
-
-// ReactDOM.createRoot(document.getElementById('root'), 
-//     <React.StrictMode>
-//         <BrowserRouter>
-//         <App/>
-//         </BrowserRouter>
-//     </React.StrictMode>
-
-// );
-ReactDOM.render(<BrowserRouter><App /> </BrowserRouter>, document.getElementById('root'));
+ReactDOM.createRoot(
+  document.getElementById("root"),
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />{" "}
+  </BrowserRouter>,
+  document.getElementById("root")
+);
